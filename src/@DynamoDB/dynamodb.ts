7cacfd44b1c24ea2e1b937a1b110acf5f0d85042
System Administrator <Besjan Sejrani>
2024-08-08 10:16:09 +0200

    modified dynamodb

--- src/@DynamoDB/dynamodb.ts
+++ src/@DynamoDB/dynamodb.ts
@@ -3,10 +3,10 @@ import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
   region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+  // credentials: {
+  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  // },
 });
 
 const docClient = DynamoDBDocumentClient.from(client);
