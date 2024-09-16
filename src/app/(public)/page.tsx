d7a1e62d67cb5d326275197ba67be2dd37556080
System Administrator <Besjan Sejrani>
2024-09-16 21:08:07 +0200

    the reward section is hidden

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,7 @@
 // Components
 import Hero from "@/Components/client/Hero";
 import GameRules from "@/Components/client/GameRules";
-import Rewards from "@/Components/client/Rewards";
+// import Rewards from "@/Components/client/Rewards";
 
 // ====================================================================================
 
@@ -12,7 +12,7 @@ const Home = () => {
 
       <GameRules />
 
-      <Rewards />
+      {/* <Rewards /> */}
     </>
   );
 };
